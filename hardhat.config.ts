6eaa84412e4bca18f57cd82edd01407a02770648
Siyang Shao <siyang.shao@outlook.com>
2023-10-31 16:03:52 +0800

    build: add dependency on @openzeppelin/contracts and change Solidity version to v0.8.20

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,12 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      evmVersion: "shanghai"
+    }
+  }
 };
 
 export default config;
