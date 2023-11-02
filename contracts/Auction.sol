11be8a68d9d151c8b2de8dbf625b5bf4948af357
Siyang Shao <siyang.shao@outlook.com>
2023-11-02 16:38:20 +0800

    fix: Dutch Auction rules
    Modify the auction code to make it conform to the Dutch auction rules.

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -1,33 +1,47 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.20;
+
+import {DUT} from "./Token.sol";
 
 contract DutchAuction {
     address public auctioneer;
     uint256 public reservePrice;
     uint256 public startPrice;
-    uint256 public startBlock;
+    uint256 public startTime;
     uint256 public priceDropInterval;
-    address public highestBidder;
-    uint256 public highestBid;
+    address[] public bidders;
+    uint256[] public bids;
+    uint256[] public amount;
+    uint256 public totalCommitment;
+    uint256 initialSupply;
 
-    enum AuctionState { Created, Running, Ended, Canceled }
+    DUT private token;
+
+    enum AuctionState {
+        Created,
+        Running,
+        Ended,
+        Canceled
+    }
     AuctionState public state;
 
     event AuctionStarted(uint startBlock);
     event NewBid(address bidder, uint amount);
-    event AuctionEnded(address winner, uint amount);
+    event AuctionEnded(address[] winner, uint[] amount);
 
     constructor(
         uint256 _startPrice,
         uint256 _reservePrice,
-        uint256 _priceDropInterval
+        uint256 _priceDropInterval,
+        uint256 _initialSupply
     ) {
         auctioneer = msg.sender;
         startPrice = _startPrice;
         reservePrice = _reservePrice;
-        startBlock = block.number;
         priceDropInterval = _priceDropInterval;
         state = AuctionState.Created;
+        initialSupply = _initialSupply;
+        token = new DUT(_initialSupply); // depoly a new contract
     }
 
     modifier onlyAuctioneer() {
@@ -36,51 +50,68 @@ contract DutchAuction {
     }
 
     modifier auctionNotEnded() {
-        require(state == AuctionState.Running);
+        require(state == AuctionState.Running, "auction is not running");
+        require(block.timestamp <= startTime + 20 minutes, "auction has been expired");
         _;
     }
 
     function startAuction() public onlyAuctioneer {
+        startTime = block.timestamp;
         state = AuctionState.Running;
-        emit AuctionStarted(startBlock);
+        emit AuctionStarted(startTime);
     }
 
     function cancelAuction() public onlyAuctioneer {
         state = AuctionState.Canceled;
     }
 
-    function currentPrice() public view returns (uint256) {
-        if (state == AuctionState.Created || state == AuctionState.Canceled) {
-            return 0;
-        }
-        uint256 elapsedBlocks = block.number - startBlock;
-        uint256 priceDrop = elapsedBlocks / priceDropInterval;
-        return (startPrice > priceDrop) ? startPrice - priceDrop : reservePrice;
+    function currentPrice() public view auctionNotEnded returns (uint256) {
+        uint256 elapsedTimes = block.timestamp - startTime;
+        uint256 priceDrop = elapsedTimes / priceDropInterval;
+        return
+            (startPrice - priceDrop > reservePrice)
+                ? startPrice - priceDrop
+                : reservePrice;
     }
 
     function bid() public payable auctionNotEnded {
         uint256 price = currentPrice();
         require(msg.value >= price);
-
-        if (highestBidder != address(0)) {
-            // Refund the previous highest bidder
-            payable(highestBidder).transfer(highestBid);
+        uint256 remainSupply = initialSupply - totalCommitment * price;
+        uint256 remainMaximumValue = remainSupply * price;
+        uint256 commitment = msg.value;
+        if (commitment > remainMaximumValue) {
+            payable(msg.sender).transfer(commitment - remainMaximumValue);
+            commitment = remainMaximumValue;
         }
-
-        highestBidder = msg.sender;
-        highestBid = price;
-
-        emit NewBid(msg.sender, price);
+        totalCommitment += msg.value;
+        bidders.push(msg.sender);
+        bids.push(msg.value);
+        emit NewBid(msg.sender, msg.value);
+        if (totalCommitment >= price * initialSupply) {
+            finalizeAuction();
+        }
     }
 
     function finalizeAuction() public {
         require(state == AuctionState.Running);
-        require(block.number >= startBlock);
-
+        require(block.timestamp >= startTime);
         state = AuctionState.Ended;
-        emit AuctionEnded(highestBidder, highestBid);
-
+        require(bidders.length == bids.length);
+        uint bidsNumber = bidders.length;
+        uint256 price = currentPrice();
+        for (uint i = 0; i < bidsNumber; i++) {
+            amount.push(bids[i] / price);
+            bids[i] -= price * amount[i];
+            token.transfer(bidders[i], amount[i]);
+            if (bids[i] > 0) {
+                payable(bidders[i]).transfer(bids[i]);
+            }
+        }
+        emit AuctionEnded(bidders, bids);
         // Transfer the highest bid to the auctioneer
-        payable(auctioneer).transfer(highestBid);
+        if (block.timestamp >= startTime + 20 minutes) {
+            token.burnAfterTime();
+        }
     }
 }
